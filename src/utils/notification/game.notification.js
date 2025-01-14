96d4dd6302a8c7e8f0cc76c30d97f03255ffad59
황종하 <whdgk3615@naver.com>
2025-01-14 15:23:20 +0900

    [fix] game.noritification
    클라이언트에게 전솔할 패킥 함수 고침

--- src/utils/notification/game.notification.js
+++ src/utils/notification/game.notification.js
@@ -1,22 +1,27 @@
-import { getProtoMessages } from "../../init/loadProtos.js";
-import { PACKET_TYPE } from "../../constants/header.js";
+import { config } from '../../config/config.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+import { PACKET_TYPE } from '../../constants/header.js';
 
+// 서버가 클라이언트에게 전송할 알림 패킷 생성 함수
 const makeNotification = (message, type) => {
-  // 패킷 길이 정보를 포함한 버퍼 생성
-  const packetLength = Buffer.alloc(4);
-  packetLength.writeUInt32BE(message.length + 1, 0); // 패킷 길이에 타입 바이트 포함
+  // TOTAL_LENGTH헤더 생성
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
 
-  // 패킷 타입 정보를 포함한 버퍼 생성
-  const packetType = Buffer.alloc(1);
+  // PACKET_TYPE헤더 생성
+  const packetType = Buffer.alloc(config.packet.typeLength);
   packetType.writeUInt8(type, 0);
 
-  // 길이 정보와 메시지를 함께 전송
   return Buffer.concat([packetLength, packetType, message]);
 };
 
+// 유저 위치 정보 패킷(수신하는 유저 제외)을 생성하는 함수
 export const createLocationPacket = (users) => {
-  const protoMessages = getProtoMessages();
-  const Location = protoMessages.gameNotification.LocationUpdate;
+  const protoMessage = getProtoMessages();
+  const Location = protoMessage.gameNotification.LocationUpdate;
 
   const payload = { users };
   const message = Location.create(payload);
